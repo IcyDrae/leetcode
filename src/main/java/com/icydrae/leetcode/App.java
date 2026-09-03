e46da0233d7f9fccf48bef902196b910abb8ac66
IcyDrae <35934418+IcyDrae@users.noreply.github.com>
2026-09-03 12:00:25 +0200

    Problem004

--- src/main/java/com/icydrae/leetcode/App.java
+++ src/main/java/com/icydrae/leetcode/App.java
@@ -3,14 +3,15 @@ package com.icydrae.leetcode;
 import com.icydrae.leetcode.Solutions.Problem001MaximumSubarray;
 import com.icydrae.leetcode.Solutions.Problem002Stocks;
 import com.icydrae.leetcode.Solutions.Problem003MaximumSubarrayProduct;
+import com.icydrae.leetcode.Solutions.Problem004ProductArrayExceptSelf;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        new Problem003MaximumSubarrayProduct()
-            .maxProduct(
-                new int[] {2, 3, -2, 4}
+        new Problem004ProductArrayExceptSelf()
+            .productExceptSelf(
+                new int[] {1, 2, 3, 4}
         );
     }
 }
